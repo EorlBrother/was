f29b183d4f7bd7aee81a1a6745cd3a70c515880e
David Magnus Henriques <eorlbruder@magnus-henriques.de>
2016-01-30 03:53:33 +0100

    grobes GameStateHandling

--- Main.hs
+++ Main.hs
@@ -9,11 +9,15 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 
 module Main where
 
+import Text.Printf
 import Graphics.UI.Fungen
 import Graphics.Rendering.OpenGL (GLdouble)
 
-data GameAttribute = Score Int
- 
+data GameAttribute = Scores Int Int -- Hier können scheinbar VIIIIELE Dinge verpackt werden, s. worms.hs in fungen/examples
+data GameState = Player Int | Calculation Int
+
+type WasAction a = IOGame GameAttribute () GameState () a
+
 width = 1920
 height = 1080
 w = fromIntegral width :: GLdouble
@@ -21,19 +25,17 @@ h = fromIntegral height :: GLdouble
 
 main :: IO ()
 main = do
-  let winConfig = ((100,80),(width,height),"A brief example!")
-      gameMap = colorMap 0.0 0.0 0.0 w h
-      bar     = objectGroup "barGroup"  [createBar]
-      ball    = objectGroup "ballGroup" [createBall]
-      initScore = Score 0
-      input = [
-        (SpecialKey KeyRight, StillDown, moveBarToRight)
-        ,(SpecialKey KeyLeft,  StillDown, moveBarToLeft)
-        ,(Char 'q',            Press,     \_ _ -> funExit)
-        ]
-  funInit winConfig gameMap [bar,ball] () initScore input gameCycle (Timer 20) []
+    let winConfig = ((100,80),(width,height),"A brief example!")
+        gameMap = colorMap 0.0 0.0 0.0 w h
+        initScore = Scores 0 0
+        initState = Player 1
+        input = [
+          (SpecialKey KeyRight, Press, \_ _ -> nextTurn)
+          ,(Char 'q',            Press,     \_ _ -> funExit)
+          ] 
+    funInit winConfig gameMap [] initState initScore input gameCycle (Timer 20) []
 
-createBall :: GameObject ()
+{-createBall :: GameObject ()
 createBall =
   let ballPic = Basic (Circle 6.0 0.0 1.0 0.0 Filled)
   in object "ball" ballPic False (w/2,h/2) (-8,8) ()
@@ -60,28 +62,28 @@ moveBarToLeft _ _ = do
   (sX,_)  <- getObjectSize obj
   if (pX - (sX/2) - 5 >= 0)
     then (setObjectPosition ((pX - 5),pY) obj)
-    else (setObjectPosition (sX/2,pY) obj)
+    else (setObjectPosition (sX/2,pY) obj)-}
 
-gameCycle :: IOGame GameAttribute () () () ()
+nextTurn :: WasAction ()
+nextTurn = do
+  gState <- getGameState
+  case gState of
+    Player n -> do
+                  setGameState (Calculation n)
+    Calculation n -> case n of 
+                    1 -> do
+                          setGameState (Player 2)
+                    2 -> do
+                          setGameState (Player 1)  
+
+
+gameCycle :: WasAction ()
 gameCycle = do
-  (Score n) <- getGameAttribute
-  printOnScreen (show n) TimesRoman24 (0,0) 1.0 1.0 1.0
-
-  ball <- findObject "ball" "ballGroup"
-  col1 <- objectLeftMapCollision ball
-  col2 <- objectRightMapCollision ball
-  when (col1 || col2) (reverseXSpeed ball)
-  col3 <- objectTopMapCollision ball
-  when col3 (reverseYSpeed ball)
-  col4 <- objectBottomMapCollision ball
-  when col4 $ do
-    -- funExit
-    setGameAttribute (Score 0)
-    reverseYSpeed ball
-
-  bar <- findObject "bar" "barGroup"
-  col5 <- objectsCollision ball bar
-  let (_,vy) = getGameObjectSpeed ball
-  when (and [col5, vy < 0])  (do reverseYSpeed ball
-                                 setGameAttribute (Score (n + 10)))
+  (Scores s1 s2) <- getGameAttribute
+  printOnScreen (show s1) TimesRoman24 (0,0) 1.0 1.0 1.0
+  printOnScreen (show s2) TimesRoman24 (20,0) 1.0 1.0 1.0
+  gState <- getGameState
+  case gState of
+      Player n -> printOnScreen ("Player " ++ show(n)) TimesRoman24 (100,100) 1.0 1.0 1.0
+      Calculation n -> printOnScreen ("Calculation " ++ show(n)) TimesRoman24 (100,100) 1.0 1.0 1.0
   showFPS TimesRoman24 (w-60,0) 1.0 0.0 0.0
---
+++ Queue.hs
@@ -0,0 +1,3 @@
+import Data.Dequeue
+
+let x = (fromList [1,2,3]) :: BankersDequeue Integer in popFront x
