60a42863d0654f2a1f40e0dabf3562fd15b46360
David Magnus Henriques <eorlbruder@magnus-henriques.de>
2016-01-31 10:34:42 +0100

    deleeeeeeeeeeeete

--- Util.hs
+++ Util.hs
@@ -22,11 +22,6 @@ genRandomInt min max = (unsafePerformIO (genRandomIntIO min max))
 genRandomList :: Int -> Int -> Int -> [Int]
 genRandomList 0 _ _= []
 genRandomList k min max= (genRandomInt min max):genRandomList (k-1) min max
----- modded from https://github.com/haskell-game/fungen/blob/master/Graphics/UI/Fungen/Objects.hs
---createPicture :: ObjectPicture -> (GameObjectPicture,Point2D)
---createPicture (Basic (Polyg points r g b fillMode))  = (B (P (point2DtoVertex3 points) (Color4 r g b 1.0) fillMode),findSize points)
---createPicture (Basic (Circle radius r g b fillMode)) = (B (C radius (Color4 r g b 1.0) fillMode),(2 * radius,2 * radius))
---createPicture (Tex size picIndex) = (Tx picIndex,size)
 
 -- taken from https://github.com/haskell-game/fungen/blob/master/Graphics/UI/Fungen/Game.hs
 loadPictures :: [(FilePath,InvList)] -> IO [TextureObject]
